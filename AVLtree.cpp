3968e24de723e12ef9f5795615037dd1755ed5be
dev123 <hyunho3445@gmail.com>
2025-02-17 20:15:10 +0900

    AVLtree fin

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -21,7 +21,7 @@ int height(AvlNode* node) {
 
 AvlNode* rotateLL(AvlNode* parent) {
 	AvlNode* child = parent->left;
-	parent = child->right;
+	parent->left = child->right;
 	child->right = parent;
 
 	parent->Height = Max(height(parent->left), height(parent->right)) + 1;
@@ -29,3 +29,135 @@ AvlNode* rotateLL(AvlNode* parent) {
 		
 	return child;
 }
+
+AvlNode* rotateRR(AvlNode* parent) {
+	AvlNode* child = parent->right;
+	parent->right = child->left;
+	child->left = parent;
+
+	parent->Height = Max(height(parent->right), height(parent->left)) + 1;
+	child->Height = Max(height(child->right), parent->Height) + 1;
+	
+	return child;
+}
+
+AvlNode* rotateLR(AvlNode* parent) {
+	AvlNode* child = parent->left;
+	parent->left = rotateRR(child);
+	return rotateLL(parent);
+}
+
+AvlNode* rotateRL(AvlNode* parent)
+{
+	AvlNode* child = parent->right;
+	parent->right = rotateLL(child);
+	return rotateRR(parent);
+}
+
+AvlNode* avlAdd(AvlNode* root, int data) {
+	if (root == NULL) {
+		root = (AvlNode*)malloc(sizeof(AvlNode));
+		if (root == NULL)
+		{
+			exit(1);
+		}
+		root->data = data;
+		root->Height = 0;
+		root->left = root->right = NULL;
+	}
+
+	else if (data < root->data)
+	{
+		root->left = avlAdd(root->left, data);
+
+		if (height(root->left) - height(root->right) == 2)
+		{
+			if (data < root->left->data)
+			{
+				root = rotateLL(root);
+			}
+			else
+			{
+				root = rotateLR(root);
+			}
+		}
+	}
+
+	else if (data > root->data)
+	{
+		root->right = avlAdd(root->right, data);
+	
+		if (height(root->right) - height(root->left) == 2)
+		{
+			if (data > root->right->data) 
+			{
+				root = rotateRR(root);
+			}
+			else 
+			{
+				root = rotateRL(root);
+			}
+		}
+	}
+
+	else {
+		printf("중복 키 오류");
+		exit(1);
+	}
+	root->Height = Max(height(root->left), height(root->right)) + 1;
+	return root;
+}
+
+AvlNode* avlSearch(AvlNode* node, int key) {
+	if (node == NULL) return NULL;
+	if (key == node->data) return node;
+	else if (key < node->data)
+	{
+		return avlSearch(node->left, key);
+	}
+	else if (key > node->data)
+	{
+		return avlSearch(node->right, key);
+	}
+}
+
+AvlNode* inorderTraveling(AvlNode* root) {
+	if (root != NULL)
+	{
+		inorderTraveling(root->left);
+		printf("[%d]", root->data);
+		inorderTraveling(root->right);
+	}
+	return 0;
+}
+
+int main() {
+	
+	AvlNode* root = NULL;
+	
+	root = avlAdd(root, 7);
+	root = avlAdd(root, 8);
+	root = avlAdd(root, 9);
+	root = avlAdd(root, 2);
+	root = avlAdd(root, 1);
+	root = avlAdd(root, 5);
+	root = avlAdd(root, 3);
+	root = avlAdd(root, 6);
+	root = avlAdd(root, 4);
+
+	printf("검색 : %d\n", avlSearch(root, 4)->data);
+	printf("\n");
+	inorderTraveling(root);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
